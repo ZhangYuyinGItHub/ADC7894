bf1aa27f62a2d16dbf3510a928a03346ad5fe0fe
yuyin_zhang <yuyin_zhang@realsil.com.cn>
2019-01-09 21:16:56 +0800

    update adc flow

--- AD7894/USER/main.c
+++ AD7894/USER/main.c
@@ -6,6 +6,8 @@
 #include "timer.h"
 #include "ad7894_driver.h"
 
+unsigned short value = 0;
+
 void delay(int time)
 {
 		 while(time--);
@@ -28,27 +30,39 @@ int main(void)
 	NVIC_Configuration();
 
 	/* adc config*/
+	/*1. init should be execute once*/
   adc_gpio_init();
 	
 	for( ; ; )
 	{	
-		  GPIO_SetBits(GPIOA, GPIO_Pin_1);		  
-		  delay(200000);
-		  delay(200000);
-			delay(200000);
-			delay(200000);
-			delay(200000);
-			delay(200000);
-			delay(200000);
-		  delay(200000);
-		  delay(200000);
-			delay(200000);
-			delay(200000);
-			delay(200000);
-		  delay(200000);
-		  delay(200000);
-		  GPIO_ResetBits(GPIOA, GPIO_Pin_1);
+		
+		  /*2. start a new adc converse*/
   		adc_start();
+		  
+		  /*3. start a new adc converse*/
+		  delay(200000);
+		  delay(200000);
+			delay(200000);
+			delay(200000);
+			delay(200000);
+			delay(200000);
+			delay(200000);
+		  delay(200000);
+		  delay(200000);
+			delay(200000);
+			delay(200000);
+			delay(200000);
+		  delay(200000);
+		  delay(200000);
+		  delay(200000);
+		  
+		
+		  /*4. get adc converse state whether finish and success*/
+		  if (ADC_STATE_SUCCESS == adc_get_state())
+			{
+				  /*5. if success, then get the adc new value*/
+				  value = adc_read_value();
+			}
 			
 	}
 }
